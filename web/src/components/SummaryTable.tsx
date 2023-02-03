419780a40df74d1aee1f3d856f00dc8b9d961fb6
Pablo George <pablogeokar@hotmail.com>
2023-02-03 20:18:15 -0300

    terminando o dia 3 web

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -23,7 +23,11 @@ export function SummaryTable() {
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map((date) => (
-          <HabitDay key={date.toString()} />
+          <HabitDay
+            amount={5}
+            completed={Math.random() * 5}
+            key={date.toString()}
+          />
         ))}
         {amountOfDaysToFill > 0 &&
           Array.from({ length: amountOfDaysToFill }).map((_, i) => (
